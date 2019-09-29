055758ec9e2cb071467f4f8403bfd178604b2b97
FruggyDK <christiankaae34@gmail.com>
2019-09-29 02:28:09 +0200

    updated stats.py

--- stats.py
+++ stats.py
@@ -40,10 +40,18 @@ class data:
 
     def printOut(self):
         self.data.sort()
-        print(self.data)
-        print(self.sum)
-        print(self.length)
-        print(self.middel)
+        print("data: ",self.data)
+        print("sum: ",self.sum)
+        print("n: ",self.length)
+        print("middel: ",self.middel)
+        print("obs: ",self.obs)
+        print("hyppighed: ",self.hyppighed)
+        print("varians: ",self.varians)
+        print("spredning: ",self.spredning)
+        print("min: ",self.min)
+        print("max: ",self.max)
+        print("variationsbredde: ",self.variationsbredde)
+
 
     def g_hyppighed(self):
         self.data.sort()
@@ -54,30 +62,30 @@ class data:
         else:
             for obs in self.obs:
                 self.hyppighed[obs] = self.data.count(obs)
-            else:
-                print(self.hyppighed)
-                for obs, hyppighed in self.hyppighed.items():
-                    print(obs, hyppighed)
+            #else:
+                #print(self.hyppighed)
+                #for obs, hyppighed in self.hyppighed.items():
+                    #print(obs, hyppighed)
 
     def g_varians(self):
         for entry in self.data:
             self.varians += math.pow((entry - self.middel),2)
         else:
             self.varians = self.varians / self.length
-            print(self.varians)
+            #print(self.varians)
 
 
     def g_spredning(self):
         self.spredning = math.sqrt(self.varians)
-        print(self.spredning)
+        #print(self.spredning)
 
 
 
-obs = data([1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3, 20])
-obs.length -= 1
-obs.printOut()
-obs.g_hyppighed()
-print(obs.hyppighed)
-obs.g_varians()
-obs.g_spredning()
-print(obs.min, obs.max, obs.variationsbredde)
+
+#obs.length -= 1
+#obs.printOut()
+#obs.g_hyppighed()
+#print(obs.hyppighed)
+#obs.g_varians()
+#obs.g_spredning()
+#print(obs.min, obs.max, obs.variationsbredde)
