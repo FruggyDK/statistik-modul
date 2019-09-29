f9e71749fee6021694b44694a1b442dca899e9ac
FruggyDK <christiankaae34@gmail.com>
2019-09-29 02:27:01 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from stats import data
+
+class input:
+    def __init__(self, input):
+        self.input = input
+
+obs = data([1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,5,1,1])
+obs.g_hyppighed()
+obs.g_varians()
+obs.g_spredning()
+obs.printOut()
+
+
